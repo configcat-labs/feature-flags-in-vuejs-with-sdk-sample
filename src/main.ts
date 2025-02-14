7ccc1221f2cb8427f1348c685fcd599638425dbe
Chavez Harris <codedbychavez@gmail.com>
2025-02-14 10:16:16 -0400

    Add code comment in `App.vue` and reformat code in `main.ts`

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { ConfigCatPlugin, PollingMode } from 'configcat-vue'
 const app = createApp(App);
 
 app.use(ConfigCatPlugin, {
-  sdkKey: "YOUR-CONFIGCAT-SDK-KEY",
+  sdkKey: "YOUR-CONFIGCAT-SDK-KEY", // Replace with your SDK key
   pollingMode: PollingMode.AutoPoll, // Optional. Default is AutoPoll
   clientOptions: {
     pollIntervalSeconds: 5 // Optional. Specify the polling interval in seconds. The default is 60 seconds.
